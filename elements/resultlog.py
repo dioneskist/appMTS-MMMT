20912f57b5edc85cfb2e42b149d0f0aa14ca544f
Dione Schubert Kist <10130193+dioneskist@users.noreply.github.com>
2023-11-28 14:02:43 -0300

    fix: fix write_excel method

--- elements/resultlog.py
+++ elements/resultlog.py
@@ -2,6 +2,7 @@ import time
 from datetime import datetime
 from elements.attempt import Attempt
 from elements.attemptlog import AttemptLog
+import xlsxwriter
 
 
 class ResultLog:
@@ -74,7 +75,67 @@ class ResultLog:
             f.write(lines)
 
     def write_excel_file(self):
-        pass
+
+        filename = self.filename.replace('csv', 'xlsx')
+        workbook = xlsxwriter.Workbook(filename)
+        worksheet = workbook.add_worksheet()
+        bold = workbook.add_format({"bold": True})
+        datahora = workbook.add_format({'num_format': 'dd/mm/yy hh:mm:ss'})
+        deltatime = workbook.add_format({'num_format': 'hh:mm:ss.000'})
+
+        worksheet.write('A1', 'Relatório de Pesquisa', bold)
+        worksheet.write('A3', 'Participante')
+        worksheet.write('B3', self.participant)
+        worksheet.write('A4', 'Data')
+        worksheet.write('B4', self.date, datahora)
+        worksheet.write('A5', 'Hora de Início')
+        worksheet.write('B5', self.start_time, datahora)
+        worksheet.write('A6', 'Hora de conclusão')
+        worksheet.write('B6', self.end_time, datahora)
+        worksheet.write('A7', 'Treino/Teste')
+        worksheet.write('B7', self.test_type)
+        worksheet.write('A9', 'Dados Teste')
+        worksheet.write('A10', 'Attemps')
+
+        worksheet.write('A11', 'Comparação')
+        worksheet.write('B11', 'Chave')
+        worksheet.write('C11', 'Modelo')
+        worksheet.write('D11', 'Chave Modelo')
+        worksheet.write('E11', 'A/E')
+        worksheet.write('F11', 'Acerto Consec.')
+        worksheet.write('G11', 'Latência')
+
+        row = 12
+        for attempt in self.attempts:
+            worksheet.write(row, 0, attempt.comparation)
+            worksheet.write_number(row, 1, int(attempt.key_comparation))
+            worksheet.write(row, 2, attempt.model)
+            worksheet.write_number(row, 3, int(attempt.key_model))
+            worksheet.write(row, 4, attempt.hit_or_error)
+            worksheet.write_number(row, 5, int(attempt.consecutive_hits))
+            worksheet.write(row, 6, attempt.latency_from_screen, deltatime)
+            row += 1
+
+        row += 1
+        worksheet.write(row, 0, 'Latência Total', bold)
+        worksheet.write(row, 1, self.latency_total, deltatime)
+        row += 1
+        worksheet.write(row, 0, 'Latência Média', bold)
+        worksheet.write(row, 1, self.latency_avg, deltatime)
+        row += 1
+        worksheet.write(row, 0, 'Total de Acertos', bold)
+        worksheet.write_number(row, 1, self.hits)
+        row += 1
+        worksheet.write(row, 0, 'Total de Erros', bold)
+        worksheet.write_number(row, 1, self.errors)
+        row += 1
+        worksheet.write(row, 0, 'Total de Pareamentos', bold)
+        worksheet.write_number(row, 1, self.pareamentos_total)
+        row += 1
+        worksheet.write(row, 0, 'Total de Pareamentos ate o critério', bold)
+        worksheet.write_number(row, 1, self.pareamentos_ate_acerto)
+        workbook.close()
+
 
 def generate_csv(attempt):
     ret = ""
