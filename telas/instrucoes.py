0ff79d2f5fed1ee47591da916dfa113edb7a4e7b
Dione S Kist <10130193+dioneskist@users.noreply.github.com>
2025-05-13 14:52:14 -0300

    Removed smile

--- telas/instrucoes.py
+++ telas/instrucoes.py
@@ -14,6 +14,6 @@ class TelaInstrucoes(Screen):
             self.ids._instrucoes.text = """
     Cada figura possui um par. Vamos descobrir quais são os pares?
 
-    Arraste e junte as figuras para formar os pares. Quando você acertar, uma carinha feliz :) aparecerá. Quando você errar, a figura voltará para o lugar.
+    Arraste e junte as figuras para formar os pares. Quando você acertar, uma carinha feliz aparecerá. Quando você errar, a figura voltará para o lugar.
 """
             logging.debug("Instrucoes printadas!!")
